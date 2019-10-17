ba2bb74483e6c29d0bbed2be71525467a5e1f60c
trungngotdt <trungngotdt@outlook.com.vn>
2019-10-17 20:50:12 +0700

    commit

--- TestLargestRectangleInBinaryMatrix/UnitTest1.cs
+++ TestLargestRectangleInBinaryMatrix/UnitTest1.cs
@@ -5,33 +5,42 @@ namespace TestLargestRectangleInBinaryMatrix
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        private void RunCommand(string cmd)
         {
             Process process = new Process();
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
             startInfo.FileName = "/bin/bash";
-            startInfo.Arguments = "-c \"pwd\"";
+            startInfo.Arguments = $"-c \"{cmd}\"";
             process.StartInfo = startInfo;
             process.Start();
+        }
+        [SetUp]
+        public void Setup()
+        {
+            
         }
 
         [Test]
         public void TestC()
         {
+            RunCommand("cd /root/project/LargestRectangleInBinaryMatrixC");
             Assert.Pass();
         }
 
         [Test]
         public void TestPython()
         {
+
+            RunCommand("cd /root/project/LargestRectangleInBinaryMatrixPy");
             Assert.Pass();
         }
 
         [Test]
         public void TestJava()
         {
+
+            RunCommand("cd /root/project/LargestRectangleInBinaryMatrixJava");
             Assert.Pass();
         }
     }
